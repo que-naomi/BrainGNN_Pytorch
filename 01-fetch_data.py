bbe08541fe15322f30bd8587ce27886c63f017bb
que-naomi <128472789+que-naomi@users.noreply.github.com>
2023-12-25 14:38:58 +0800

    Update 01-fetch_data.py

--- 01-fetch_data.py
+++ 01-fetch_data.py
@@ -27,7 +27,7 @@ import sys
 
 # Input data variables
 code_folder = os.getcwd()
-root_folder = '/data/'
+root_folder = '/content/BrainGNN_Pytorch/data/'
 data_folder = os.path.join(root_folder, 'ABIDE_pcp/cpac/filt_noglobal/')
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
